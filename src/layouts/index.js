1405f520dc02c0625380ac4a45f4bcdfae465560
Trina Castillo <trina.castillo@expeditors.com>
2018-02-23 15:50:49 -0800

    refactor(app): remove extra content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import Helmet from 'react-helmet';
 
 import Navbar from '../components/Navbar';
 import './all.sass';
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({ data, children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title={data.site.siteMetadata.title} />
     <Navbar />
     <div>{children()}</div>
   </div>
 );
 
+export const pageQuery = graphql`
+  query TemplateWrapperQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
 TemplateWrapper.propTypes = {
+  data: PropTypes.any,
   children: PropTypes.func,
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import Link from "gatsby-link";
-import Script from "react-load-script";
-import graphql from "graphql";
+import React from 'react';
+import Script from 'react-load-script';
 
 export default class IndexPage extends React.Component {
   handleScriptLoad() {
@@ -18,9 +16,6 @@ export default class IndexPage extends React.Component {
   }
 
   render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
-
     return (
       <section className="section">
         <Script
@@ -29,54 +24,10 @@ export default class IndexPage extends React.Component {
         />
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">Hello World</h1>
           </div>
-          {posts
-            .filter(post => post.node.frontmatter.templateKey === "blog-post")
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.frontmatter.path}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
         </div>
       </section>
     );
   }
 }
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-            path
-          }
-        }
-      }
-    }
-  }
-`;
