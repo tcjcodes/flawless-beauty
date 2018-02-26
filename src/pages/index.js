3fcfcc411afe8f31b0891ac793b1831d4a87e7c6
Trina Castillo <trina.castillo@expeditors.com>
2018-02-26 11:50:35 -0800

    WIP styling index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Script from 'react-load-script';
+import './_index-page.scss';
+
+import './index';
+
+const GridRow = ({ children }) => (<div className='flex-container'>{children}</div>);
+
+const GridItem = ({ children, color }) => (
+  <div className={`flex-cell landing-box box-${color}`}>
+    <div className='flex-item landing-box-cell'>
+      {children}
+    </div>
+  </div>
+);
 
 export default class IndexPage extends React.Component {
   handleScriptLoad() {
     if (typeof window !== `undefined` && window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
+      window.netlifyIdentity.on('init', user => {
         if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
           });
         }
       });
@@ -17,15 +30,22 @@ export default class IndexPage extends React.Component {
 
   render() {
     return (
-      <section className="section">
+      <section className='fb-index-page section'>
         <Script
-          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          url='https://identity.netlify.com/v1/netlify-identity-widget.js'
           onLoad={() => this.handleScriptLoad()}
         />
-        <div className="">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Hello World</h1>
-          </div>
+        <div className='container text-center'>
+          <GridRow>
+            <GridItem color='pink'>LipSense</GridItem>
+            <GridItem color='gray' />
+            <GridItem color='pink'>ShadowSense</GridItem>
+          </GridRow>
+          <GridRow>
+            <GridItem color='gray' />
+            <GridItem color='pink'>Shop</GridItem>
+            <GridItem color='gray' />
+          </GridRow>
         </div>
       </section>
     );
