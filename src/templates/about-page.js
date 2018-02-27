2e8ddfe5610c5475bb795f8fb8c6b5c944bdab24
Cathryn <tcjcastillo@gmail.com>
2018-02-27 15:03:55 -0800

    about page v2

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,73 +1,58 @@
 import React from 'react';
 import graphql from 'graphql';
 import Content, { HTMLContent } from '../components/Content';
-import { cx } from 'emotion';
-import cImg from '../img/flowerbrushes.jpeg';
-import eImg from '../img/flowerbrushes.jpeg';
+import img from '../img/aboutus.jpeg';
 import './_about.scss';
 import styled from 'react-emotion';
 
-const CircleContainer = styled('div')`
+const BoxContainer = styled('div')`
   display: inline-block;
   position: relative;
-  margin-bottom: 1em;
+  margin-top: 1.5em;
 
   &:before {
     display: block;
     content: '';
     position: absolute;
-    top: 6px;
-    right: 6px;
-    bottom: 6px;
-    left: 6px;
-    
-    border: 1px solid white;
-    border-radius: 50%;
+    top: -10px;
+    right: -10px;
+    bottom: 10px;
+    left: 10px;
+
+    border: 2px solid ${props => props.theme.gold};
   }
 `;
-const CircleImage = styled('img')`
+const BoxImage = styled('img')`
   vertical-align: middle;
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
+  width: 300px;
 `;
 
-const Avatar = ({ img }) => (
-  <CircleContainer>
-    <CircleImage src={img} />
-  </CircleContainer>
-);
-
-const ColumnCard = ({ name, img, children }) => (
-  <div className={cx('d-flex', 'column col-5 col-mx-auto')}>
-    <div className="card">
-      <div className="card-body">
-        <Avatar img={img} />
-        <div>
-          <h3 className="fb-script">{name}</h3>
-          <div>{children}</div>
-        </div>
-      </div>
-    </div>
-  </div>
+const Avatar = () => (
+  <BoxContainer>
+    <BoxImage src={img} />
+  </BoxContainer>
 );
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
   return (
-    <div className="fb-about-page fb-section container grid-md text-center">
-      <h1 className="fb-page-title">About Us</h1>
+    <div className="fb-about-page fb-section container">
       <div className="columns">
-        <ColumnCard name="Edilene" img={eImg}>
+        <div className="column col-4 col-ml-auto">
+          <Avatar img={img}/>
+        </div>
+        <div className="column col-6 col-mr-auto">
+          <h1 className="fb-page-title">About Us</h1>
+
+          <h3 className="fb-script">Edilene</h3>
           <p>
             Single-origin coffee is coffee grown within a single known
             geographic origin. Sometimes, this is a single farm or a specific
             collection of beans from a single country. The name of the coffee is
             then usually the place it was grown to whatever degree available.
           </p>
-        </ColumnCard>
 
-        <ColumnCard name="Christine" img={cImg}>
+          <h3 className="fb-script">Christine</h3>
           <p>
             Coffee is a small tree or shrub that grows in the forest understory
             in its wild form, and traditionally was grown commercially under
@@ -75,7 +60,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
             coffee farms provides habitat for a great number of migratory and
             resident species.
           </p>
-        </ColumnCard>
+        </div>
       </div>
     </div>
   );
