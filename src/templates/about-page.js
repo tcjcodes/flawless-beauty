ec6ed44f7e30c6afdfeef4e678a15b34d41001bb
Cathryn <tcjcastillo@gmail.com>
2018-02-27 14:30:00 -0800

    styling about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,30 +1,88 @@
-import React from 'react'
-import graphql from 'graphql'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import graphql from 'graphql';
+import Content, { HTMLContent } from '../components/Content';
+import { cx, css } from 'emotion';
+import cImg from '../img/aboutc.jpg';
+import eImg from '../img/aboute.jpeg';
+import './_about.scss';
+import styled from 'react-emotion';
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+const CircleContainer = styled('div')`
+  display: inline-block;
+  position: relative;
+  margin-bottom: 1em;
 
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
+  &:before {
+    display: block;
+    content: '';
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    bottom: 6px;
+    left: 6px;
+    
+    border: 1px solid white;
+    border-radius: 50%;
+  }
+`;
+const CircleImage = styled('img')`
+  vertical-align: middle;
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+`;
+
+const Avatar = ({ img }) => (
+  <CircleContainer>
+    <CircleImage src={img} />
+  </CircleContainer>
+);
+
+const ColumnCard = ({ name, img, children }) => (
+  <div className={cx('d-flex', 'column col-5 col-mx-auto')}>
+    <div className="card">
+      <div className="card-body">
+        <Avatar img={img} />
+        <div>
+          <h3 className="fb-script">{name}</h3>
+          <div>{children}</div>
         </div>
       </div>
-    </section>
-  )
-}
+    </div>
+  </div>
+);
+
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
+  return (
+    <div className="fb-about-page container grid-md text-center">
+      <h1 className="fb-page-title">About Us</h1>
+      <div className="columns">
+        <ColumnCard name="Edilene" img={eImg}>
+          <p>
+            Single-origin coffee is coffee grown within a single known
+            geographic origin. Sometimes, this is a single farm or a specific
+            collection of beans from a single country. The name of the coffee is
+            then usually the place it was grown to whatever degree available.
+          </p>
+        </ColumnCard>
+
+        <ColumnCard name="Christine" img={cImg}>
+          <p>
+            Coffee is a small tree or shrub that grows in the forest understory
+            in its wild form, and traditionally was grown commercially under
+            other trees that provided shade. The forest-like structure of shade
+            coffee farms provides habitat for a great number of migratory and
+            resident species.
+          </p>
+        </ColumnCard>
+      </div>
+    </div>
+  );
+};
 
 export default ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <AboutPageTemplate
@@ -32,8 +90,8 @@ export default ({ data }) => {
       title={post.frontmatter.title}
       content={post.html}
     />
-  )
-}
+  );
+};
 
 export const aboutPageQuery = graphql`
   query AboutPage($path: String!) {
@@ -45,4 +103,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
--- src/templates/what-page.js
+++ src/templates/what-page.js
@@ -31,48 +31,50 @@ export const WhatPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
-    <div className={cx(`container grid-lg`, css`margin-bottom: 4em;`)}>
-      <h1 className='text-center center-block fb-page-title'>{title}</h1>
-      <div className="columns">
-        <div className={`column col-4 mr-2 pr-2`}>
-          <BorderedImageBox bg={lipsenseImg}>
-            <BorderBox margin="-10px 10px" />
-          </BorderedImageBox>
-        </div>
-        <div className={cx(`column col-6 d-flex ${flexRowVAlign}`)}>
-          <div>
-            <h2 className="fb-script">LipSense</h2>
-            <p>
-              Coffee is a small tree or shrub that grows in the forest
-              understory in its wild form, and traditionally was grown
-              commercially under other trees that provided shade. The
-              forest-like structure of shade coffee farms provides habitat for a
-              great number of migratory and resident species.
-            </p>
+    <div className="fb-what-page">
+      <div className={cx(`container grid-lg`, css`margin-bottom: 4em; background: #fff;  height: 100%;`)}>
+        <h1 className='text-center center-block fb-page-title'>{title}</h1>
+        <div className="columns">
+          <div className={`column col-4 mr-2 pr-2`}>
+            <BorderedImageBox bg={lipsenseImg}>
+              <BorderBox margin="-10px 10px" />
+            </BorderedImageBox>
+          </div>
+          <div className={cx(`ml-2 column col-6 d-flex ${flexRowVAlign}`)}>
+            <div>
+              <h2 className="fb-script">LipSense</h2>
+              <p>
+                Coffee is a small tree or shrub that grows in the forest
+                understory in its wild form, and traditionally was grown
+                commercially under other trees that provided shade. The
+                forest-like structure of shade coffee farms provides habitat for a
+                great number of migratory and resident species.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="columns">
-        <div
-          className={cx(
-            `column col-6 col-ml-auto d-flex px-2 ${flexRowVAlign}`,
-          )}
-        >
-          <div>
-            <h2 className="fb-script">ShadowSense</h2>
-            <p>
-              Single-origin coffee is coffee grown within a single known
-              geographic origin. Sometimes, this is a single farm or a specific
-              collection of beans from a single country. The name of the coffee
-              is then usually the place it was grown to whatever degree
-              available.
-            </p>
+        <div className="columns">
+          <div
+            className={cx(
+              `column col-6 col-ml-auto d-flex px-2 ${flexRowVAlign}`,
+            )}
+          >
+            <div>
+              <h2 className="fb-script">ShadowSense</h2>
+              <p>
+                Single-origin coffee is coffee grown within a single known
+                geographic origin. Sometimes, this is a single farm or a specific
+                collection of beans from a single country. The name of the coffee
+                is then usually the place it was grown to whatever degree
+                available.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className={`column col-4 px-2`}>
-          <BorderedImageBox bg={shadowsenseImg}>
-            <BorderBox margin="-10px -10px" />
-          </BorderedImageBox>
+          <div className={`column col-4 px-2`}>
+            <BorderedImageBox bg={shadowsenseImg}>
+              <BorderBox margin="-10px -10px" />
+            </BorderedImageBox>
+          </div>
         </div>
       </div>
     </div>
