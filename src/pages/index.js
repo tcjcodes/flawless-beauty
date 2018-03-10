4ecfa703b668b5d9458af6a5f0b18b943dfc59a3
Trina Castillo <trina.castillo@expeditors.com>
2018-03-09 18:17:16 -0800

    linked home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import { css, cx } from 'emotion';
+import Link from 'gatsby-link';
 import React from 'react';
 import styled from 'react-emotion';
 import Script from 'react-load-script';
+import LayoutSection from '../components/LayoutSection';
 import bg3 from '../img/eyesense.jpeg';
 import bg0 from '../img/eyeshadow.jpeg';
 import goldBg from '../img/goldgradient.jpeg';
 import bg2 from '../img/katemaxlips.jpg';
 import bg1 from '../img/redlips2.jpeg';
 import './_index-page.scss';
-
 import './index';
-import LayoutSection from '../components/LayoutSection';
 
 const GridRow = ({ children }) => (
   <div className="flex-container">{children}</div>
@@ -69,21 +69,25 @@ export default class IndexPage extends React.Component {
           <GridRow>
             <GridItem bg={bg0} />
             <GridItem fontSize="2em">
-              Shop<br />All
+              <Link to="/products">View<br/>All</Link>
             </GridItem>
           </GridRow>
           <GridRow>
             <GridItem bg={bg1} />
             <GridItem fontSize="1.75em">
-              What<br />is it?
+              <Link to="/what">What<br />is it?</Link>
             </GridItem>
           </GridRow>
           <GridRow>
-            <GridItem fontSize="2.25em">LIPS</GridItem>
+            <GridItem fontSize="2.25em">
+              <Link to="/products#lips">Lips</Link>
+            </GridItem>
             <GridItem bg={bg2} />
           </GridRow>
           <GridRow>
-            <GridItem fontSize="2.25em">EYES</GridItem>
+            <GridItem fontSize="2.25em">
+              <Link to="/products#eyes">Eyes</Link>
+            </GridItem>
             <GridItem bg={bg3} />
           </GridRow>
         </LayoutSection>
