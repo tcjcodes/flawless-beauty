c945e1d4b07097ad681cae1ad35c565fc6940821
Trina Castillo <trina.castillo@expeditors.com>
2018-03-12 09:19:36 -0700

    removed gatsby image and sharp dependents

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
         name: 'images',
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    // 'gatsby-plugin-sharp',
+    // 'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
